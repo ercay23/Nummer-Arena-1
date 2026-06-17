bd4d9a40c0bddf7eddb7491c1bbb26061b799cdc
ercay23 <ercanayin23@gmail.com>
2026-06-17 11:37:39 +0200

    Add number display functionality to script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Number Arena çalışıyor");
+const numbers = [2,4,7,8,9,75];
+
+const container = document.getElementById("balls");
+
+numbers.forEach(number => {
+
+    const ball = document.createElement("div");
+
+    ball.className = "ball";
+
+    ball.textContent = number;
+
+    container.appendChild(ball);
+
+});
